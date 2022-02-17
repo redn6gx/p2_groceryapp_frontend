fb99ebadfda1073bd1e61f52fe6beb53df7f7f7a
Bobby <bodavis@csumb.edu>
2022-02-17 08:27:23 -0800

    Added foundation for shopping cart

--- javascripts/shoppingcart.js
+++ javascripts/shoppingcart.js
@@ -1,4 +1,8 @@
-function test(){
-    let header = document.getElementById("header1");
-    header.innerHTML = "HI";
-}
+/**
+ * Retrieve all data from the current user's cart
+ * Append each item to the main cart divider
+ *      Remove button: Remove this item from cart
+ *      Quantity: Number input that can be changed
+ * Update current state of cart when user leaves page
+ * Checkout button: Redirect to checkout page 
+ */
