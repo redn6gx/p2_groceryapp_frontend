c849d98e80d06fcc3ea268ea97a5407176833ec0
Art Weinstein <artur.weinstein@gmail.com>
2022-02-21 15:50:14 -0800

    refactor carthistory

---
+++ javascripts/shoppingcart.js
@@ -0,0 +1,119 @@
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', getCurrentCart);
+} else {
+    getCurrentCart();
+}
+
+currentUserId = 2; //get from local storage
+currentCartId = 1; //get from local storage
+toBeRemoved = [];
+
+async function getCurrentCart(){
+    let url = `http://localhost:8080/carthistory/current/${currentUserId}`;
+    let httpResponse = await fetch(url);
+    let requestBody = await httpResponse.json();
+    currentCartId = requestBody.id;
+
+    console.log(requestBody.id);
+}
+
+async function getCurrentCartItems(){
+    let url = `http://localhost:8080/cartitem/${currentCartId}`;
+    let httpResponse = await fetch(url);
+    let requestBody = await httpResponse.json();
+
+    originalCartItems = {};
+    for(let index in requestBody){
+        originalCartItems[index] = requestBody[index];
+    }
+
+    appendItems(requestBody);
+}
+
+function appendItems(cartItems){
+    var index = 0;
+    for(let currCartItem of cartItems){
+        let [imgPath, itemName, price] = [currCartItem.item.img_path, currCartItem.item.itemName, currCartItem.item.price];
+        let currentRow = document.createElement("tr");
+        let cartTable = document.querySelector(".cart-table");
+
+        currentRow.classList.add("cart-row");
+        currentRow.setAttribute("id", index);
+        currentRow.innerHTML = 
+            `<tr>
+                <td><img src="${imgPath}" alt="" <span>${itemName}</span></td>
+                <td><span>$${price.toFixed(2)}</span></td>
+                <td><input class="qty" type="number" min="1" value="${currCartItem.quantity}"></td>
+                <td><button class="button-delete">Remove Item</button></td>
+            </tr>`;
+        cartTable.appendChild(currentRow);
+        currentRow.getElementsByClassName("button-delete")[0].addEventListener("click", removeItem);
+        currentRow.getElementsByClassName("qty")[0].addEventListener("change", updateQuantity)
+        index++;
+    }
+    updateTotal();
+}
+
+function removeItem(event){
+    var buttonClicked = event.target;
+    var parentId = buttonClicked.parentElement.parentElement.id;
+    toBeRemoved.push(originalCartItems[parentId].id);
+    delete originalCartItems[parentId];
+    buttonClicked.parentElement.parentElement.remove();
+    updateTotal();
+}
+
+function updateQuantity(event){
+    var qtyChanged = event.target;
+    var parentId = qtyChanged.parentElement.parentElement.id;
+    originalCartItems[parentId].quantity = qtyChanged.value;
+    updateTotal();
+}
+
+function updateTotal(){
+    var result = 0;
+    var itemCount = 0;
+    for(let key in originalCartItems){
+        result += originalCartItems[key].quantity * originalCartItems[key].item.price;
+        itemCount += parseInt(originalCartItems[key].quantity);
+    }
+    document.getElementById("total").innerHTML = "Subtotal (" + itemCount + " items): " + result;
+    document.getElementsByClassName("cart-headers")[0].style.display = (itemCount == 0) ? "none" : "table-row";
+}
+
+async function updateCartState(event){
+    for(let key in originalCartItems){
+        let url = `http://localhost:8080/cartitem/${originalCartItems[key].id}`;
+
+        const data = {
+            id: originalCartItems[key].id,
+            quantity: originalCartItems[key].quantity,
+            cartHistory: originalCartItems[key].cartHistory,
+            item: originalCartItems[key].item
+        }
+
+        const httpResponse = await fetch(url, {
+            method: "PUT",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+
+        const body = await httpResponse.json();
+    }
+
+    for(let cartItemId of toBeRemoved){
+        let url = `http://localhost:8080/cartitem/${cartItemId}`;
+
+        const httpResponse = await fetch(url, {
+            method: "DELETE",
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+    }
+
+    toBeRemoved = [];
+}
+
